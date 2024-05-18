3017bc6eae633f6c7d27267c36df99b14a0857a7
Ciczau <wiktor.michalski@outlook.com>
2024-05-18 22:14:31 +0200

    feat: recommended page

---
+++ app/src/app/containers/RecommendedPage/RecommendedPage.styled.tsx
@@ -0,0 +1,77 @@
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+`;
+
+export const Container = styled.div`
+  width: calc(100% - 90px);
+  padding: 40px;
+`;
+export const Header = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Content = styled.div`
+  margin-top: 40px;
+`;
+
+export const Activity = styled.div`
+  box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.25);
+  padding: 20px;
+  margin-top: 20px;
+  transition: 0.2s ease-in;
+  transform: scale(1);
+  &:hover {
+    transform: scale(1.01);
+  }
+`;
+export const PaginationContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  position: absolute;
+  bottom: 15px;
+  left: 50%;
+`;
+
+export const PageButton = styled.button<{ active?: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  font-size: 18px;
+  font-weight: 700;
+  height: 40px;
+  margin: 0 5px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  background-color: ${(props) => (props.active ? "#ddd" : "white")};
+  color: ${(props) => (props.active ? "white" : "#333")};
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: #eee;
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+`;
+
+export const PrevButton = styled(IoIosArrowBack)`
+  height: 20px;
+  width: 20px;
+`;
+
+export const NextButton = styled(IoIosArrowForward)`
+  height: 20px;
+  width: 20px;
+`;
---
+++ app/src/app/containers/RecommendedPage/RecommendedPage.tsx
@@ -0,0 +1,73 @@
+import Navigation from "@/app/components/Navigation/Navigation";
+import * as S from "./RecommendedPage.styled";
+import Typography from "@/app/components/Typography/Typography";
+import Input from "@/app/components/Input/Input";
+import { useRouter } from "next/router";
+
+const mockActivites = [
+  {
+    city: "Zduńska Wola",
+    activities: ["Przejażdżka rowerowa z dziećmi", "Spacer po parku"],
+  },
+  {
+    city: "Henryków",
+    activities: ["Spacer po lesie"],
+  },
+];
+
+const SearchPage = () => {
+  const router = useRouter();
+  const location_id = router.query.location_id as string;
+
+  const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      router.push("/search/" + e.currentTarget.value);
+    }
+  };
+
+  const renderActivities = () => {
+    if (mockActivites.length === 0) {
+      return <Typography tag="h2">Brak aktywności w danym miejscu.</Typography>;
+    } else {
+      return mockActivites.map((activityByCity: any) => {
+        return (
+          <S.Activity key={`search-page-${activityByCity.city}`}>
+            <Typography tag="h4">{activityByCity.city}</Typography>
+            {activityByCity.activities.map((activity: any) => {
+              return (
+                <div key={`search-page-${activityByCity.city}-${activity}`}>
+                  <Typography tag="h5">{activity}</Typography>
+                </div>
+              );
+            })}
+          </S.Activity>
+        );
+      });
+    }
+  };
+  return (
+    <S.Wrapper>
+      <Navigation />
+      <S.Container>
+        <S.Header>
+          <div>
+            <Typography tag="h2">Polecane aktywności dla Ciebie</Typography>
+          </div>
+          <div />
+        </S.Header>
+        <S.Content>{renderActivities()}</S.Content>
+        <S.PaginationContainer>
+          <S.PageButton>
+            <S.PrevButton />
+          </S.PageButton>
+          <S.PageButton active>1</S.PageButton>
+          <S.PageButton>
+            <S.NextButton />
+          </S.PageButton>
+        </S.PaginationContainer>
+      </S.Container>
+    </S.Wrapper>
+  );
+};
+
+export default SearchPage;
