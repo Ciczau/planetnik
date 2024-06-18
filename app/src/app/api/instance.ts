a581d15bc7fd952cd57738af20ed62b5e5850ec9
Ciczau <wiktor.michalski@outlook.com>
2024-06-18 23:51:15 +0200

    chore: api url

--- app/src/app/api/instance.ts
+++ app/src/app/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://planetnik-backend-7ff91dff2443.herokuapp.com",
   headers: {
     "Content-Type": "application/json",
   },
